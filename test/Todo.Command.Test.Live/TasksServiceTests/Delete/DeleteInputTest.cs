a74893683ee609265c169ac357e9f490f1f8a0ea
EmadAbushofa <emad.abushofa@gmail.com>
2022-07-15 20:38:20 +0200

    Add integration command and query tests

---
+++ test/Todo.Command.Test.Live/TasksServiceTests/Delete/DeleteInputTest.cs
@@ -0,0 +1,88 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Todo.Command.Abstractions;
+using Todo.Command.Events;
+using Todo.Command.Test.Client.TodoProto;
+using Todo.Command.Test.Fakers.Created;
+using Todo.Command.Test.Helpers;
+using Todo.Command.Test.Live.EventBus;
+using Todo.Command.Test.Live.Helpers;
+using Xunit.Abstractions;
+
+namespace Todo.Command.Test.Live.TasksServiceTests.Delete
+{
+    public class DeleteInputTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public DeleteInputTest(WebApplicationFactory<Program> factory, ITestOutputHelper helper)
+        {
+            _factory = factory.WithDefaultConfigurations(helper, services =>
+            {
+                services.AddServiceBusListener();
+            });
+        }
+
+        [Fact]
+        public async Task Delete_SendValidRequest_TaskDeletedEventSaved()
+        {
+            var createdEvent = await GenerateAndAppendToStreamAsync();
+
+            var client = new Tasks.TasksClient(_factory.CreateGrpcChannel());
+
+            var request = new DeleteRequest()
+            {
+                Id = createdEvent.AggregateId.ToString(),
+                UserId = createdEvent.UserId,
+            };
+
+            var response = await client.DeleteAsync(request);
+
+            var stream = _factory.Services.GetRequiredService<IEventStore>();
+
+            var @events = await stream.GetStreamAsync(response.Id);
+
+            Assert.Equal(2, events.Count);
+            AssertEquality.OfDeletedEvent(events[1], request, response, expectedSequence: 2);
+        }
+
+        [Fact]
+        public async Task Delete_SendValidRequest_TaskDeletedEventPublished()
+        {
+            var listener = _factory.Services.GetRequiredService<TodoCommandListener>();
+
+            var createdEvent = await GenerateAndAppendToStreamAsync();
+
+            var client = new Tasks.TasksClient(_factory.CreateGrpcChannel());
+
+            var request = new DeleteRequest()
+            {
+                Id = createdEvent.AggregateId.ToString(),
+                UserId = createdEvent.UserId,
+            };
+
+            var response = await client.DeleteAsync(request);
+
+            listener.Events.Clear();
+
+            await Task.Delay(8000);
+
+            await listener.CloseAsync();
+
+            Assert.Equal(2, listener.Events.Count);
+            AssertEquality.OfDeletedEvent(listener.Events[1], request, response, expectedSequence: 2);
+        }
+
+        private async Task<TaskCreated> GenerateAndAppendToStreamAsync()
+        {
+            var eventStore = _factory.Services.GetRequiredService<IEventStore>();
+
+            var taskCreatedEvent = new TaskCreatedFaker()
+                .Generate();
+
+            await eventStore.AppendToStreamAsync(taskCreatedEvent);
+
+            return taskCreatedEvent;
+        }
+    }
+}
---
+++ test/Todo.Query.Test.Live/HandlersTests/TaskDeletedTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Todo.Query.Test.Fakers;
+using Todo.Query.Test.Fakers.Deleted;
+using Todo.Query.Test.Helpers;
+using Todo.Query.Test.Live.Client.DemoEventsProto;
+using Todo.Query.Test.Live.Helpers;
+using Xunit.Abstractions;
+
+namespace Todo.Query.Test.HandlersTests
+{
+    public class TaskDeletedTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+        private readonly DbContextHelper _dbContextHelper;
+
+        public TaskDeletedTest(WebApplicationFactory<Program> factory, ITestOutputHelper helper)
+        {
+            _factory = factory.WithDefaultConfigurations(helper);
+            _dbContextHelper = new DbContextHelper(_factory.Services);
+        }
+
+
+        [Fact]
+        public async Task When_NewTaskDeletedEventArrived_TaskSaved()
+        {
+            var todoTask = await _dbContextHelper.InsertAsync(new TodoTaskFaker().Generate());
+
+            var @event = new TaskDeletedFaker()
+                .For(todoTask).Generate();
+
+            var client = CommandServiceHelper.CreateDemoEventsClient();
+
+            await client.DeleteAsync(new DeleteRequest()
+            {
+                Id = @event.AggregateId.ToString(),
+                UserId = @event.UserId,
+                Sequence = @event.Sequence,
+            });
+
+            await Task.Delay(5000);
+
+            todoTask = await _dbContextHelper.Query(c => c.Tasks.FindAsync(@event.AggregateId));
+
+            Assert.Null(todoTask);
+        }
+    }
+}
