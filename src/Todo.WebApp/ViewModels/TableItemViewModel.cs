eefc6a0f78e57b71c09a4024f8ac75a8075c5939
EmadAbushofa <emad.abushofa@gmail.com>
2022-07-29 02:32:37 +0200

    Authorize and call filter API in todos page

--- src/Todo.WebApp/ViewModels/TableItemViewModel.cs
+++ src/Todo.WebApp/ViewModels/TableItemViewModel.cs
@@ -1,10 +1,20 @@
-﻿namespace Todo.WebApp.ViewModels
+﻿using Todo.WebApp.Models;
+
+namespace Todo.WebApp.ViewModels
 {
     public class TableItemViewModel
     {
+        public TableItemViewModel(TodoTaskFilterOutput output)
+        {
+            Id = output.Id;
+            Title = output.Title;
+            DueDate = output.DueDate.ToString("yyyy-MM-dd");
+            IsCompleted = output.IsCompleted;
+            DuplicateTitle = output.DuplicateTitle;
+        }
+
         public string? Id { get; set; }
         public string? Title { get; set; }
-        public string? Note { get; set; }
         public string? DueDate { get; set; }
         public bool IsCompleted { get; set; }
         public bool DuplicateTitle { get; set; }
